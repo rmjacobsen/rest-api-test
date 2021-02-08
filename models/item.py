e5889e3660d84cc2482a5ef53701375409cf89dc
Rasmus <rasmusjacobsen94@gmail.com>
2021-02-08 17:03:18 -0300

    fixed the item

--- models/item.py
+++ models/item.py
@@ -16,7 +16,7 @@ class ItemModel(db.Model):
         self.store_id = store_id
 
     def json(self):
-        return {'name': self.name, 'price': self.price, 'store_id'}
+        return {'name': self.name, 'price': self.price}
     
     @classmethod
     def find_by_name(cls, name):
